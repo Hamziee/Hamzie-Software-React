a03ee6be84ebed4d795ae63d45da8e781e0428e7
Hamziee <91210085+Hamziee@users.noreply.github.com>
2022-07-07 20:27:55 +0200

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './style.css'
+import Page from './views/page'
 import ComingSoon from './views/coming-soon'
 import Aboutus from './views/aboutus'
 import Home from './views/home'
-import Page from './views/page'
 
 const App = () => {
   return (
     <Router>
       <div>
+        <Route exact component={Page} path="/page" />
         <Route exact component={ComingSoon} path="/coming-soon" />
         <Route exact component={Aboutus} path="/aboutus" />
         <Route exact component={Home} path="/" />
-        <Route exact component={Page} path="/page" />
       </div>
     </Router>
   )
